2be2b1d1ae5335b85c18f03bdbd3dd78134131cd
Tyler J. Wagner <tyler@tolaris.com>
2019-04-03 10:13:50 +0100

    Add copyright Google LLC, my employer.

--- pokemongo_game_master_to_csv.py
+++ pokemongo_game_master_to_csv.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+# Copyright 2019 Google LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
 # CSV row headers
 headerFastMoves = ["Move Name", "Type", "DPT", "EPT", "D+EPT", "PvP Duration", "PvP Power", "PvP Energy", "PvE Power", "PvE Energy", "PvE Duration"]
 headerChargeMoves = ["Move Name", "Type", "PvP Power", "PvP Energy", "PvP DPE", "PvE Power", "PvE Energy", "PvE Duration"]
